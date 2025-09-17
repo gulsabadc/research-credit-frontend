e40005437407d7ad2a37c53cdfb7c6b548bda832
Arif Ur Rahman <arifurrahman.it.doc@gmail.com>
2025-09-17 12:55:40 +0600

    Glowing effect at Dasboard Image Added

--- src/components/Dashboard-preview.tsx
+++ src/components/Dashboard-preview.tsx
@@ -1,9 +1,13 @@
 export default function DashboardPreview() {
   return (
     <div className="relative flex justify-center items-center py-20">
-      {/* Glowing effect at the top */}
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-96 h-32 bg-cyan-400/20 rounded-full blur-3xl"></div>
+      
+      {/* <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-96 h-36 bg-purple-400 rounded-bl-lg blur-3xl"></div> */}
 
+<div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-96 h-36">
+  <div className="w-full h-full bg-indigo-500 rounded-bl-lg blur-3xl opacity-100"></div>
+  <div className="w-full h-full bg-purple-200 rounded-bl-lg blur-2xl opacity-50 -mt-32"></div>
+</div>
       {/* Dashboard image with specified dimensions */}
       <div className="relative">
         <img
